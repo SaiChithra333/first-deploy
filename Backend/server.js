be2aa2be4eae4e6eba1990313ac19ae1009f814b
Sai Chithra <saichithrarao@gmail.com>
2024-10-18 21:24:02 +0530

    frontendurl added

--- Backend/server.js
+++ Backend/server.js
@@ -6,13 +6,13 @@ const cors = require('cors');
 dotenv.config();
 
 // Allow requests from your frontend origin
-const corsOptions = {
-    origin: 'https://dont-knowfront-jvuswsv71-sai-chithras-projects.vercel.app', // Update to your frontend URL
-    optionsSuccessStatus: 200,
-    credentials: true // If you are using cookies for authentication
-};
+app.use(
+    cors({
+      origin: process.env.FRONTEND_URL, // Frontend origin
+      credentials: true,               // Allow cookies to be sent
+    })
+  );
 
-app.use(cors(corsOptions));
 
 
 app.use(express.json());
